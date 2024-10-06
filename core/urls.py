e9ba3fa55d6db7fff54ddf0d514b8c169fcebbf4
WalterMelon~ <vgberny@gmail.com~>
2024-10-05 19:22:04 -0600

    faltan conversaciones

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import HomePageView, SamplePageView
+from django.contrib import admin
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('sample/', SamplePageView.as_view(), name="sample"),
+    path('admin/', admin.site.urls),
 ]

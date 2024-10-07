811f4ec266c8bcb0e8103061602ec2cfcafedaf5
Berny Valverde <140218473+Walt3rMel0n@users.noreply.github.com>
2024-10-07 15:30:47 -0600

    Update views.py

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ class HomePageView(TemplateView):
     template_name = "core/home.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title':"Mi super web Playground"})
+        return render(request, self.template_name, {'title':"Playground"})
 
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"

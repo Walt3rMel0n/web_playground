9238d9179469eda0e3081088d969df1e404834e1
WalterMelon~ <vgberny@gmail.com~>
2024-10-09 11:18:39 -0600

    debug

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['web-production-c5fb.up.railway.app','localhost','https://web-production-c5fb.up.railway.app', '127.0.0.1']
